738319d8816dfdf258299049abb9f9e0364eeb38
wartis <kirill.klenichev@yandex.ru>
2020-02-09 12:13:22 +0300

    hyperjump

---
+++ src/com/wartis/introduction/Hyperjump.java
@@ -0,0 +1,19 @@
+package com.wartis.introduction;
+
+import java.util.Scanner;
+
+public class Hyperjump {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in, "ISO-8859-1");
+        int n = scanner.nextInt();
+        int maxSum = 0;
+        int curSum = 0;
+        for (int i=0; i<n; i++){
+            int curNum = scanner.nextInt();
+            curSum = Math.max((curSum + curNum), 0);
+            if (curSum > maxSum) maxSum = curSum;
+        }
+        System.out.println(maxSum);
+    }
+}
